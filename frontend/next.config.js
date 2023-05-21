a01f498972cbf920d4949f811fbc73eb186bd1cf
Leo Reid <leo.reid.mail@gmail.com>
2023-05-21 15:14:31 +0200

    changed frontend ip to prod ip

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     // exposed api key on the frontend, but no one is really going to use the site anyway, so it's probably fine.
     ELEVEN_LABS_API_KEY: 'a01a9bc2477ef196c75c80f8d2ae568b',
-    BACKEND_URL: 'http://localhost:5000/'
+    BACKEND_URL: 'https://backend-tmhm8.ondigitalocean.app/'
   }
 }
 
