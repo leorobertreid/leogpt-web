d010d545f5bb7d8e77c55c6f0743522e1a0bc43a
Leo Reid <leo.reid.mail@gmail.com>
2023-05-23 06:49:52 +0200

    changed backend url on frontend to be reserved ip

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     ELEVEN_LABS_API_KEY: 'a01a9bc2477ef196c75c80f8d2ae568b',
     // BACKEND_URL: 'https://backend-tmhm8.ondigitalocean.app/',
     // BACKEND_URL: 'http://localhost:5000/'
-    BACKEND_URL: 'http://138.68.111.124:5000/',
+    BACKEND_URL: 'http://146.190.178.214:5000/',
   }
 }
 
